b2859dd0d4389e65cbcf879aefdaf40fbfaab06c
Evgeny Bovykin <rebovykin@gmail.com>
2015-10-14 14:01:32 +0300

    Add legend.position

--- src/api.legend.js
+++ src/api.legend.js
@@ -6,6 +6,29 @@ c3_chart_fn.legend.show = function (targetIds) {
 };
 c3_chart_fn.legend.hide = function (targetIds) {
     var $$ = this.internal;
-    $$.hideLegend($$.mapToTargetIds(targetIds));
+    $$.hideLegend(targetIds);
     $$.updateAndRedraw({withLegend: true});
 };
+
+c3_chart_fn.legend.position = function(position) {
+    var $$ = this.internal;         
+
+    if (!arguments.length) {        
+        if (!$$.config.legend_show) {   
+            position = 'off';               
+        } else if ($$.isLegendRight) {  
+            position = 'right';             
+        } else if ($$.isLegendInset) {  
+            position = 'inset';             
+        } else {   
+            position = 'bottom';            
+        }          
+        return position;                
+    }              
+
+    $$.isLegendRight = position === 'right';
+    $$.isLegendInset = position === 'inset';
+
+    $$.cachedRedraw({withLegend: true});
+};
+
