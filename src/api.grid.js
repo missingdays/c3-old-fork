0b7694eb8ef6174ebdccc2645c5877967d0dcc4d
Evgeny Bovykin <rebovykin@gmail.com>
2015-10-14 14:22:46 +0300

    Move gridX and gridY

--- src/api.grid.js
+++ src/api.grid.js
@@ -29,3 +29,82 @@ c3_chart_fn.ygrids.remove = function (params) { // TODO: multiple
     var $$ = this.internal;
     $$.removeGridLines(params, false);
 };
+
+c3_chart_fn.gridX = function() {
+    var $$ = this.internal;       
+    return $$.config.grid_x_show; 
+};               
+
+c3_chart_fn.gridX.show = function() {
+    var $$ = this.internal;       
+    $$.config.grid_x_show = true; 
+
+    if (isUndefined($$.xgrid)){
+        return;
+    }
+
+    var gridx = $$.xgrid[0];      
+    for (var id in gridx) {
+        if(gridx[id]){
+            gridx[id].style.display = "";
+        }
+    }
+
+    $$.updateAndRedraw({withLegend: true});
+};
+
+c3_chart_fn.gridX.hide = function() {
+    var $$ = this.internal;
+    $$.config.grid_x_show = false;
+
+    if (isUndefined($$.xgrid)){
+        return;
+    }
+
+    var gridx = $$.xgrid[0];
+    for (var id in gridx) {
+        if(gridx[id]){
+            gridx[id].style.display = "none";
+        }
+    }
+
+    $$.updateAndRedraw({withLegend: true});
+};
+
+c3_chart_fn.gridY = function() {
+    var $$ = this.internal;
+    return $$.config.grid_y_show;
+
+};
+
+c3_chart_fn.gridY.show = function() {
+    var $$ = this.internal;
+    $$.config.grid_y_show = true;
+
+    if (isUndefined($$.ygrid)){
+        return;
+    }
+
+    var gridy = $$.ygrid[0];
+    for (var id in gridy) {
+        gridy[id].style.display = "";
+    }
+
+    $$.updateAndRedraw({withLegend: true});
+};
+
+c3_chart_fn.gridY.hide = function() {
+    var $$ = this.internal;
+    $$.config.grid_y_show = false;
+
+    if (isUndefined($$.ygrid)){
+        return;
+    }
+
+    var gridy = $$.ygrid[0];
+    for (var id in gridy) {
+        gridy[id].style.display = "none";
+    }
+
+    $$.updateAndRedraw({withLegend: true});
+};
